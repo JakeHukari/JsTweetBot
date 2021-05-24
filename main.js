e7a5fda0941871f49abafb746103c2e2e3511171
Krypt0c <58837465+Krypt0c@users.noreply.github.com>
2021-05-23 23:05:32 -0600

    sda

---
+++ main.js
@@ -0,0 +1,16 @@
+const Twit = require('twit')
+const notifier = require('node-notifier');
+const open = require('open');
+const franc = require('franc')
+
+CONSUMER_KEY ="hzGQU7JazJT19Y79d2DrfnTJV"
+CONSUMER_SECRET = "lAQlNXoaxyEequZHnitroGSyjOSlAxB6pc76qlIHyLMn1MTJTF"   
+ACCESS_KEY = "1161775799596388352-f1e26ZYaJiL5XX7mX31l3t3VIggMPq"    
+ACCESS_SECRET = "ii8UUZ6wuUOgKd5NfMccvDDmzRO4sf41J53f3xq2eGiaH"
+
+var T = new Twit({
+  consumer_key:     CONSUMER_KEY,
+  consumer_secret:  CONSUMER_SECRET,
+  access_key:       ACCESS_KEY,
+  access_secret:    ACCESS_SECRET,
+}); 
