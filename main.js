f34b783c5529c41e45aa4446c988e7f807c99df2
Krypt0c <58837465+Krypt0c@users.noreply.github.com>
2021-05-23 23:16:14 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,17 @@
-const Twit = require('twit')
-const notifier = require('node-notifier');
-const open = require('open');
-const franc = require('franc')
-
-CONSUMER_KEY ="hzGQU7JazJT19Y79d2DrfnTJV"
-CONSUMER_SECRET = "lAQlNXoaxyEequZHnitroGSyjOSlAxB6pc76qlIHyLMn1MTJTF"   
-ACCESS_KEY = "1161775799596388352-f1e26ZYaJiL5XX7mX31l3t3VIggMPq"    
-ACCESS_SECRET = "ii8UUZ6wuUOgKd5NfMccvDDmzRO4sf41J53f3xq2eGiaH"
+var Twit = require('twit')
 
 var T = new Twit({
-  consumer_key:     CONSUMER_KEY,
-  consumer_secret:  CONSUMER_SECRET,
-  access_key:       ACCESS_KEY,
-  access_secret:    ACCESS_SECRET,
-}); 
+  consumer_key:         '...',
+  consumer_secret:      '...',
+  access_token:         '...',
+  access_token_secret:  '...',
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+  strictSSL:            true,     // optional - requires SSL certificates to be valid.
+})
 
-T.post('statuses/update', { status: 'Twit Test' }, function(err, data, response) {
-    console.log(data)
+//
+//  tweet 'hello world!'
+//
+T.post('statuses/update', { status: 'hello world!' }, function(err, data, response) {
+  console.log(data)
+})
