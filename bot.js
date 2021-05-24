5480c7a89f705806416ad0a0ef6a225893023ba7
Krypt0c <58837465+Krypt0c@users.noreply.github.com>
2021-05-24 13:40:36 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,6 +9,8 @@ var T = new Twit({
   strictSSL:            true,
 })
 
+const random = Math.floor(Math.random() * 999999999999999999999);
+
 T.post('statuses/update', { status: (random) }, function(err, data, response) {
   console.log(data)
   console.log(random)
