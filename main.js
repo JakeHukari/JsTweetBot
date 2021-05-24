d9f60f7f4a3ec7ab087f1600e4164b10bb8bb0cf
Krypt0c <58837465+Krypt0c@users.noreply.github.com>
2021-05-23 23:17:05 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 var Twit = require('twit')
 
 var T = new Twit({
-  consumer_key:         '...',
-  consumer_secret:      '...',
-  access_token:         '...',
-  access_token_secret:  '...',
+  consumer_key:         'hzGQU7JazJT19Y79d2DrfnTJV',
+  consumer_secret:      'lAQlNXoaxyEequZHnitroGSyjOSlAxB6pc76qlIHyLMn1MTJTF',
+  access_token:         '1161775799596388352-f1e26ZYaJiL5XX7mX31l3t3VIggMPq',
+  access_token_secret:  'ii8UUZ6wuUOgKd5NfMccvDDmzRO4sf41J53f3xq2eGiaH',
   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
   strictSSL:            true,     // optional - requires SSL certificates to be valid.
 })
