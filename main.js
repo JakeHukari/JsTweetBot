3244cabb1776274d242995e076febfbdc1b8a11d
Krypt0c <58837465+Krypt0c@users.noreply.github.com>
2021-05-23 23:24:41 -0600

    npm

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-const random = Math.random();
 const { randomBytes } = require('tweetnacl')
 var Twit = require('twit')
 
@@ -13,6 +12,6 @@ var T = new Twit({
 
 const random = Math.random() * 20;
 
-T.post('statuses/update', { status: 'Twit Test' }{random}, function(err, data, response) {
+T.post('statuses/update', { status: 'Twit Test'(random) }, function(err, data, response) {
   console.log(data)
 })
