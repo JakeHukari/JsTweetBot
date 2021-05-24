f227f99e25fd3b8d562cc738c45db4b97d11ff73
Krypt0c <58837465+Krypt0c@users.noreply.github.com>
2021-05-24 13:32:10 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,10 @@
 var Twit = require('twit')
 
 var T = new Twit({
-  consumer_key:         'hzGQU7JazJT19Y79d2DrfnTJV',
-  consumer_secret:      'lAQlNXoaxyEequZHnitroGSyjOSlAxB6pc76qlIHyLMn1MTJTF',
-  access_token:         '1161775799596388352-f1e26ZYaJiL5XX7mX31l3t3VIggMPq',
-  access_token_secret:  'ii8UUZ6wuUOgKd5NfMccvDDmzRO4sf41J53f3xq2eGiaH',
+  consumer_key:         'XXXX',
+  consumer_secret:      'XXXX',
+  access_token:         'XXXX',
+  access_token_secret:  'XXXX',
   timeout_ms:           60*1000,
   strictSSL:            true,
 })
